84eec057997a30a0dc68f57f8cb5cbe0af626a79
chnaphol12345-sketch <chnaphol12345@gmail.com>
2026-01-22 09:34:34 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var glob = require('glob');
+
+var mainWindow = null;
+
+// Require and setup each JS file in the main-process dir
+glob('main-process/**/*.js', function (error, files) {
+  if (error) return console.log(error);
+  files.forEach(function (file) {
+    require('./' + file).setup();
+  });
+});
+
+function createWindow () {
+  mainWindow = new BrowserWindow({ width: 920, height: 900 });
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
